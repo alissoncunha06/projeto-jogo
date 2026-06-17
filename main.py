d567ce67ff89ef504fa22d7e0a788c89d65cb2bd
alissoncunha06 <158228524+alissoncunha06@users.noreply.github.com>
2026-06-16 22:03:34 -0300

    Update main.py

--- main.py
+++ main.py
@@ -18,6 +18,8 @@ fonte = pygame.font.SysFont("Arial", 14, bold=True)
 player = pygame.Rect(60, 282, 36, 36)
 player_speed = 4.0
 
+inimigo_x = 50.0
+inimigo_y = 50.0
 inimigo = pygame.Rect(50, 50, 36, 36)
 inimigo_speed = 2.0
 
@@ -36,25 +38,28 @@ while True:
     if keys[pygame.K_UP] or keys[pygame.K_w]: player.y -= player_speed
     if keys[pygame.K_DOWN] or keys[pygame.K_s]: player.y += player_speed
 
-    dx = player.x - inimigo.x
-    dy = player.y - inimigo.y
+    dx = player.x - inimigo_x
+    dy = player.y - inimigo_y
     distancia = math.hypot(dx, dy)
-
+    
     if distancia > 0:
-        inimigo.x += (dx / distancia) * inimigo_speed
-        inimigo.y += (dy / distancia) * inimigo_speed
+        
+        inimigo_x += (dx / distancia) * inimigo_speed
+        inimigo_y += (dy / distancia) * inimigo_speed
+        
+        inimigo.x = int(inimigo_x)
+        inimigo.y = int(inimigo_y)
     
     screen.fill((28,28,38))
 
     pygame.draw.rect(screen, (200, 20, 20), player)
+    texto_puc = fonte.render("PUCPR", True, (255, 255, 255))
+    puc_x = player.x + (36 - texto_puc.get_width()) // 2
+    screen.blit(texto_puc, (puc_x, player.y + 40))
 
     pygame.draw.rect(screen, (0, 80, 200), inimigo)
-
-    nome = fonte.render("UniCesumar", True, (255, 255, 255))
-    screen.blit(nome, (inimigo.x - 10, inimigo.y + 40))
-
-    texto = fonte.render("PUC", True, (255, 255, 255))
-
-    screen.blit(texto, (player.x + 4, player.y + 11))
+    texto_uni = fonte.render("UniCesumar", True, (255, 255, 255))
+    uni_x = inimigo.x + (36 - texto_uni.get_width()) // 2
+    screen.blit(texto_uni, (uni_x, inimigo.y + 40))
 
     pygame.display.flip()
